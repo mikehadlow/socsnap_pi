5b91f635582b5167d7101926129f00b153d419aa
Mike Hadlow <mike@suteki.co.uk>
2013-08-21 09:16:08 +0000

    factored out oauth header building code.

---
+++ src/auth.h
@@ -0,0 +1,6 @@
+#ifndef _auth_h
+#define _auth_h
+
+bstring get_oauth_header(char *url, char *method);
+
+#endif
--- src/socsnap.c
+++ src/socsnap.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <oauth.h>
 #include <string.h>
 #include <curl/curl.h>
 #include <time.h>
@@ -12,7 +11,7 @@
 #include "dbg.h"
 #include "bstrlib.h"
 #include "cJSON.h"
-#include "../../twitter/twitter.h"
+#include "auth.h"
 
 #define KEYVALUE_LENGTH 7 
 #define TWITTER_HANDLE "socsnap"
@@ -55,116 +54,6 @@ static int s_send (void *socket, char *string)
 {
     int size = zmq_send (socket, string, strlen (string), 0);
     return size;
-}
-
-typedef struct bKeyValue {
-    bstring key;
-    bstring value;
-} bKeyValue;
-
-typedef struct bKeyValues {
-    int qty;
-    bKeyValue *entry;
-} bKeyValues;
-
-void destroy_keyvalues(bKeyValues *keyvalues)
-{
-    int i = 0;
-    for(i = 0; i < keyvalues->qty; i++) {
-        bdestroy(keyvalues->entry[i].key);
-        bdestroy(keyvalues->entry[i].value);
-    }
-    free(keyvalues->entry);
-    free(keyvalues);
-}
-
-bstring get_oauth_args(char *url, char *method)
-{
-    char *consumer_key = CONSUMER_KEY;
-    char *consumer_secret = CONSUMER_SECRET;
-    char *access_token = ACCESS_TOKEN;
-    char *access_token_secret = ACCESS_TOKEN_SECRET;
-
-    char *signed_url = NULL;
-    char *postarg = NULL;
-
-    signed_url = oauth_sign_url2(
-        url, 
-        &postarg, 
-        OA_HMAC, 
-        method, 
-        consumer_key, 
-        consumer_secret, 
-        access_token, 
-        access_token_secret);
-
-    free(signed_url);
-
-    bstring bpostarg = bfromcstr(postarg);
-    free(postarg);
-    return bpostarg; 
-}
-
-bKeyValues *get_key_values(const_bstring input)
-{
-    int i = 0;
-    struct bstrList *values;
-    struct bstrList *keyvalue;
-    bKeyValues *keyvalues;
-
-    values = bsplit(input, '&');
-    keyvalues = malloc(sizeof(bKeyValues));
-    keyvalues->entry = malloc(values->qty * sizeof(bKeyValue));
-    keyvalues->qty = values->qty;
-
-    for(i = 0; i < values->qty; i++) {
-        keyvalue = bsplit(values->entry[i], '=');
-        if(keyvalue->qty == 2) {
-            keyvalues->entry[i].key = bstrcpy(keyvalue->entry[0]);
-            keyvalues->entry[i].value = bstrcpy(keyvalue->entry[1]);
-        } else {
-            printf("Invalid keyvalue: %s", values->entry[i]->data);
-            return NULL;
-        }
-        bstrListDestroy(keyvalue);
-    }
-
-    bstrListDestroy(values);
-
-    return keyvalues;
-}
-
-bstring build_oauth_header(bKeyValues *keyvalues)
-{
-    bstring buffer = bfromcstr("Authorization: OAuth ");
-    int i = 0;
-    int first = 1;
-    
-    for(i = 0; i < keyvalues->qty; i++) {
-        if(first) {
-            first = 0;
-        } else {
-            bconchar(buffer, ',');
-        }
-        bconcat(buffer, keyvalues->entry[i].key);
-        bcatcstr(buffer, "=\"");
-        bconcat(buffer, keyvalues->entry[i].value);
-        bconchar(buffer, '\"');
-    }
-    return buffer;
-}
-
-bstring get_oauth_header(char *url, char *method)
-{
-    bstring args = get_oauth_args(url, method);
-    bKeyValues *keyvalues;
-    keyvalues = get_key_values(args);
-    bstring oauth_header = build_oauth_header(keyvalues);
-
-    bdestroy(args);
-    destroy_keyvalues(keyvalues);
-
-    return oauth_header;
 }
 
 void send_status(void *zcontext, char *twitterhandle)
